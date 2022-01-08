8ef8d3bb258ab31e0a6abecdc1a07a0b312672cc
jmarcosdias <jmsdias6@gmail.com>
2022-01-08 20:43:15 +0000

    Create a function to read a dictionary file
    This function is used to read games_settings.dict and
    games_dictionary.dict files.

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import json
+
+def read_dictionary_file(filename):
+    """
+    Reads a file that contains a dictionary.
+    Returns the dictionary loaded into a dict object.
+    """
+    try:
+        with open(filename, "r") as file:
+            dict_str = file.read()
+        loaded_dict = json.loads(dict_str)
+        return loaded_dict
+    except Exception:
+        print(f"\nError when reading the {filename} file.\nPlease contact the Support Team.\n")
+        raise
+
+print("Welcome to the Wonderful Words game!\n")
+
+print("Loading game settings...")
+game_settings = read_dictionary_file("game_settings.dict")
+print("Game settings loaded.")
+print(f"Game Dictionary Name: \"{game_settings['game_dictionary_name']}\".\n")
+
+print(f"Loading the \"{game_settings['game_dictionary_name']}\" dictionary...")
+game_dictionary = read_dictionary_file("game_dictionary.dict")
+print(f"\"{game_settings['game_dictionary_name']}\" dictionary loaded.\n")
+
+print("I just need to ask you two questions and the game will start!")
+player_name = input("\nWhat is your name?\n")
+n_questions = input(
+    "\nHow many questions would you like to have in your game session"
+    f" ({game_settings['minimum_number_of_questions_per_game_session']} to "
+    f" {game_settings['maximum_number_of_questions_per_game_session']})?\n")
+
+print(f"\n{player_name}, I am starting a new game session with "
+      f"{n_questions} questions for you...")
